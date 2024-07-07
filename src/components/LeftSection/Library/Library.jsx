901df1899bfe035a05c2019fc9aa0fa4c138328f
“Nayan” <nayanunni95@gmail.com>
2024-07-07 19:11:29 +0530

    complete left section

--- src/components/LeftSection/Library/Library.jsx
+++ src/components/LeftSection/Library/Library.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import { Icon } from '../../../Icons';
-import { NavLink } from 'react-router-dom';
 import styles from './Library.module.css';
+import VerticalCard from '../VerticalCard/VerticalCard';
 
 function Library() {
   return (
     <div className={styles.librarySection}>
-      <NavLink to="/library">
-        <div className={styles.libraryLabel}>
-          <div>
-            <Icon name="library" size={25} />
-          </div>
-          <div className={styles.librarySpanDiv}>
-            <span>library</span>
-          </div>
+      <div className={styles.libraryLabel}>
+        <div>
+          <Icon name="library" size={25} />
         </div>
-      </NavLink>
-      <div className={styles.libraryVerticalScroll}>
-        {/* <div className={styles.verticalCard}>
-          <img
-            src="https://i.scdn.co/image/ab67616d00004851495ce6da9aeb159e94eaa453"
-            alt=""
-          />
-        </div> */}
+        <div className={styles.librarySpanDiv}>
+          <span>library</span>
+        </div>
       </div>
+      <VerticalCard />
     </div>
   );
 }
